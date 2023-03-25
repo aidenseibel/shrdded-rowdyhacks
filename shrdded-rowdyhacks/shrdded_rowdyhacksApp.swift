a801398e62ca1631021e3adf7fb0472848572630
aidenseibel <aiden.seibel@gmail.com>
2023-03-25 12:06:47 -0500

    Initial Commit

---
+++ shrdded-rowdyhacks/shrdded_rowdyhacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  shrdded_rowdyhacksApp.swift
+//  shrdded-rowdyhacks
+//
+//  Created by Aiden Seibel on 3/25/23.
+//
+
+import SwiftUI
+
+@main
+struct shrdded_rowdyhacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
